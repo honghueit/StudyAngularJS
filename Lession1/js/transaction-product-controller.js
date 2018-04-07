1b58c2c28eaa0ed44a66e5c7efdda7ea1cb9103d
johnlenon <jlmwebhosting@gmail.com>
2018-04-07 21:42:50 +0800

    Updated transaction product controller.

--- Lession1/js/transaction-product-controller.js
+++ Lession1/js/transaction-product-controller.js
@@ -1,15 +1,17 @@
-.controller( "ProductController" , [
-	"$scope",
+angular
+	.module( "myapp" )
+	.controller( "ProductController" , [
+		"$scope",
 
-	function controller( $scope ) {
-		$scope.displayListProduct = function displayListProduct( product ) {
-			$scope.transaction.listProduct.forEach( function onEachItemproduct( _product ) {
-				if( _product.reference != product.reference ) {
-					_product._viewMode = false;
-				}
-			} );
+		function controller( $scope ) {
+			$scope.displayListProduct = function displayListProduct( product ) {
+				$scope.transaction.listProduct.forEach( function onEachItemproduct( _product ) {
+					if( _product.reference != product.reference ) {
+						_product._viewMode = false;
+					}
+				} );
 
-			product._viewMode =! product._viewMode;
-		};
-	}
-] )
+				product._viewMode =! product._viewMode;
+			};
+		}
+	] )
